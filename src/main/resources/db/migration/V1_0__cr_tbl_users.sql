443909b08c4f7cdfad3a8c212cb1efff2b8d00bf
erasmatov <erqa@live.com>
2024-01-11 22:46:20 +0400

    changed column role table users

--- src/main/resources/db/migration/V1_0__cr_tbl_users.sql
+++ src/main/resources/db/migration/V1_0__cr_tbl_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     username   VARCHAR(32)   NOT NULL UNIQUE,
     password   VARCHAR(2048) NOT NULL,
-    role       VARCHAR(32)   NOT NULL,
+    role       VARCHAR(16)   NOT NULL,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
     status     VARCHAR(8)
